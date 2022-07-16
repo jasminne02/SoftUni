fa61c2aab38be7b648d1ff30412870cd0a224ab7
jasminne02 <jasmine.yabari02@gmail.com>
2022-07-16 19:36:25 +0300

    New commit

---
+++ Python OOP/PolymorphismAnsAbstraction/Shapes.py
@@ -0,0 +1,40 @@
+import math
+from abc import ABC, abstractmethod
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.__radius = radius
+
+    def calculate_area(self):
+        return math.pi * self.__radius ** 2
+
+    def calculate_perimeter(self):
+        return 2 * math.pi * self.__radius
+
+
+class Rectangle(Shape):
+    def __init__(self, height, width):
+        self.__height = height
+        self.__width = width
+
+    def calculate_area(self):
+        return self.__width * self.__height
+
+    def calculate_perimeter(self):
+        return 2 * (self.__height + self.__width)
+
+
+circle = Circle(5)
+print(circle.calculate_area())
+print(circle.calculate_perimeter())
