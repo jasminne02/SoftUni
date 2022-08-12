3fcd65e06b0f05184ad91663dd05313b9813622f
jasminne02 <jasmine.yabari02@gmail.com>
2022-08-12 18:30:02 +0300

    New commit

--- Python OOP/Exams/10.04.2022/Structure/project/supply/drink.py
+++
@@ -1,6 +0,0 @@
-from project.supply.supply import Supply
-
-
-class Drink(Supply):
-    def __init__(self, name: str, energy=15):
-        super().__init__(name, energy)
--- Python OOP/Exams/10.04.2022/Structure/project/supply/food.py
+++
@@ -1,6 +0,0 @@
-from project.supply.supply import Supply
-
-
-class Food(Supply):
-    def __init__(self, name: str, energy=25):
-        super().__init__(name, energy)
