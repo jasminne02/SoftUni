0eafbe71ff82a34372a74e7c57b76cf39e333b9e
jasminne02 <jasmine.yabari02@gmail.com>
2022-09-22 23:05:39 +0300

    New commit

--- Python Web Basics/Django Introduction/django101/django101/tasks/urls.py
+++ Python Web Basics/Django Introduction/django101/django101/tasks/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from django101.tasks.views import show_bare_minimum_view, show_all_tasks, index
 
 urlpatterns = (
-# http://localhost:8000/tasks/
+    # http://localhost:8000/tasks/
     path('', index),
-    # http://localhost:8000/tasks/it-works
-    path('it-works', show_bare_minimum_view),
     # http://localhost:8000/tasks/all
     path('all/', show_all_tasks),
+    # http://localhost:8000/tasks/it-works
+    path('it-works', show_bare_minimum_view),
 )
--- Python Web Basics/Django Introduction/django101/django101/tasks/views.py
+++ Python Web Basics/Django Introduction/django101/django101/tasks/views.py
@@ -1,29 +1,35 @@
 from django import http
-
 from django.shortcuts import render
-
 from django101.tasks.models import Task
 
 
-def show_bare_minimum_view(request):
-    return http.HttpResponse('It works')
-
-
-def show_all_tasks(request):
-    all_tasks = Task.objects\
-        .order_by('id')\
-        .all()
-    result = ', '.join(f'{t.name}({t.id})' for t in all_tasks)
-    return http.HttpResponse(result)
-
-
+# Shows task template
 def index(request):
-    all_tasks = Task.objects \
-        .order_by('id') \
-        .all()
     context = {
-        'title': 'The tasks app!',
-        'tasks': all_tasks
+        'title': 'The tasks app',
     }
 
     return render(request, 'index.html', context)
+
+
+# Shows all tasks ordered by id
+def show_all_tasks(request):
+    all_tasks = Task.objects \
+        .order_by('id') \
+        .all()
+
+    context = {
+        'title': 'All tasks:',
+        'tasks': all_tasks
+    }
+
+    return render(request, 'index_all.html', context)
+
+
+# Shows 'It Works!' template
+def show_bare_minimum_view(request):
+    contex = {
+        'title': 'It Works!'
+    }
+
+    return render(request, 'index_it_works.html', contex)
